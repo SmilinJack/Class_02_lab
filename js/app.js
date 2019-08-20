4492cd6fbd5c3b69e122df72edb933773922ec2b
jgfrancis <jack.g.francis@gmail.com>
2019-08-19 17:32:32 -0700

    added states

--- js/app.js
+++ js/app.js
@@ -1,65 +1,99 @@
 'use strict';
 
-var haveKids = prompt('Do I have any children?');
+//var haveKids = prompt('Do I have any children?');
 // likesGameOfThrones = likesGameOfThrones.toLowerCase();
 
-if(haveKids.toUpperCase() === 'YES' || haveKids.toUpperCase() === 'Y'){
-  alert('YES!! I have 5 children and 2 stepchildren');
-} else {
-  alert('I wonder what my life would be like without my kids?');
-} console.log(haveKids);
+// if(haveKids.toUpperCase() === 'YES' || haveKids.toUpperCase() === 'Y'){
+//   alert('YES!! I have 5 children and 2 stepchildren');
+// } else {
+//   alert('I wonder what my life would be like without my kids?');
+// } console.log(haveKids);
 
-var haveGrandkids = prompt('Do I have any grandkids?');
+// var haveGrandkids = prompt('Do I have any grandkids?');
 
-if(haveGrandkids.toUpperCase() === 'YES' || haveGrandkids.toUpperCase() === 'Y'){
-  alert('I have 5 grandchildren. 3 granddaughters and 2 grandsons.');
-} else {
-  alert('Maybe I am older than I look?');
-} console.log(haveGrandkids);
+// if(haveGrandkids.toUpperCase() === 'YES' || haveGrandkids.toUpperCase() === 'Y'){
+//   alert('I have 5 grandchildren. 3 granddaughters and 2 grandsons.');
+// } else {
+//   alert('Maybe I am older than I look?');
+// } console.log(haveGrandkids);
 
-var careerChange = prompt('Coding is a career change from the Army. Did I retire?');
+// var careerChange = prompt('Coding is a career change from the Army. Did I retire?');
 
-if(careerChange.toUpperCase() === 'Yes' || careerChange.toUpperCase() === 'Y') {
-  alert('You know me better than I thought.');
-} else {
-  alert('I retired after serving 1992-2012');
-} console.log(careerChange);
+// if(careerChange.toUpperCase() === 'Yes' || careerChange.toUpperCase() === 'Y') {
+//   alert('You know me better than I thought.');
+// } else {
+//   alert('I retired after serving 1992-2012');
+// } console.log(careerChange);
 
-var leisureTime = prompt('Do I fish?');
+// var leisureTime = prompt('Do I fish?');
 
-if(leisureTime.toUpperCase() === 'YES' || leisureTime.toUpperCase() === 'Y'){
-  alert('Even when I don\'t get a bite my time on the water is the best.');
-} else {
-  alert('I don\'t catch, but I do fish.');
-} console.log(leisureTime);
+// if(leisureTime.toUpperCase() === 'YES' || leisureTime.toUpperCase() === 'Y'){
+//   alert('Even when I don\'t get a bite my time on the water is the best.');
+// } else {
+//   alert('I don\'t catch, but I do fish.');
+// } console.log(leisureTime);
 
-var likesGameOfThrones = prompt('Do I like Game Of Thrones?');
+// var likesGameOfThrones = prompt('Do I like Game Of Thrones?');
 
-if(likesGameOfThrones.toUpperCase() === 'YES' || likesGameOfThrones.toUpperCase() === 'Y'){
-  alert('Trick question. I have only seen the first episode.');
-} else {
-  alert('Trick question. I have only seen the first episode.');
-} console.log(likesGameOfThrones);
+// if(likesGameOfThrones.toUpperCase() === 'YES' || likesGameOfThrones.toUpperCase() === 'Y'){
+//   alert('Trick question. I have only seen the first episode.');
+// } else {
+//   alert('Trick question. I have only seen the first episode.');
+// } console.log(likesGameOfThrones);
 
 //6th Question
 
-var pcsMoves = prompt('In my military service, I was assigned to many different bases. Can you guess how many?'); //9
+var mypcsMoves = 7;
 
-if(pcsMoves.toUpperCase() === 'YES' || pcsMoves.toUpperCase() === 'Y'){
-  alert('YES!! I have 5 children and 2 stepchildren');
-} else {
-  alert('I wonder what my life would be like without my kids?');
+for(var i = 0; i < 4; i++){
+  var pcsMoves = prompt('In my military service, I was assigned to many different bases. Can you guess how many?');
+  pcsMoves = parseInt(pcsMoves);
+  if(pcsMoves === mypcsMoves){
+    alert('You got it!');
+    break;
+  } else if (i === 3){
+    alert('I\'ve been assigned to Forts Drum, Hood, Bragg, Gordon, Lee, Lewis, and Camp Garryowen.');
+  
+  } else if (pcsMoves < mypcsMoves) {
+    alert('Too low.');
+  } else {
+    alert('Like Seth Rogen, You\'re Too high')
+  }
 } console.log(pcsMoves);
 
 //7th Question
 
-var statesLivedin = prompt('I have lived in 9 different states. Can you guess them?');
+var statesIlivedin = ['Illinois', 'California', 'Utah', 'New York', 'Texas', 'North Carolina', 'Georgia', 'Virginia', 'Washington'];
+var userScore = 0;
+//var userScore = parseInt(userScore);
+// outer for loop keeps track of the number of guesses
+for (var j = 0; j < 3; j++) {
+  var userStateGuess = prompt('Can you guess three states that I have lived in?');
 
-if(statesLivedin.toUpperCase() === 'YES' || statesLivedin.toUpperCase() === 'Y'){
-  alert('I have 5 grandchildren. 3 granddaughters and 2 grandsons.');
-} else {
-  alert('Maybe I am older than I look?');
-} console.log(haveGrandkids);
+  // normalizing the data
+  userStateGuess = userStateGuess.toLowerCase();
+  var isCorrect = false;
+
+  // inner for loop is keeping track of cities
+  for (var k = 0; k < statesIlivedin.length; k++) {
+    if (userStateGuess === statesIlivedin[k]) {
+      isCorrect = true;
+    }
+  }
+
+  // if(condition is true){
+    //do the thing
+  // }
+  if (isCorrect) {
+    alert('You got it!');
+    userScore++;
+    break;
+  }
+  else {
+    alert('Lets try it again');
+  }
+}
+alert('You score is: ' + userScore + ' out of 9');
 
 // if(condition is met){
 //   do something
