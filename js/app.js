6f1e1d48caa669cf97c0bd21344b99b41ed81893
jgfrancis <jack.g.francis@gmail.com>
2019-08-19 17:36:28 -0700

    removed commented out lines

--- js/app.js
+++ js/app.js
@@ -1,45 +1,45 @@
 'use strict';
 
-//var haveKids = prompt('Do I have any children?');
-// likesGameOfThrones = likesGameOfThrones.toLowerCase();
+var haveKids = prompt('Do I have any children?');
+likesGameOfThrones = likesGameOfThrones.toLowerCase();
 
-// if(haveKids.toUpperCase() === 'YES' || haveKids.toUpperCase() === 'Y'){
-//   alert('YES!! I have 5 children and 2 stepchildren');
-// } else {
-//   alert('I wonder what my life would be like without my kids?');
-// } console.log(haveKids);
+if(haveKids.toUpperCase() === 'YES' || haveKids.toUpperCase() === 'Y'){
+  alert('YES!! I have 5 children and 2 stepchildren');
+} else {
+  alert('I wonder what my life would be like without my kids?');
+} console.log(haveKids);
 
-// var haveGrandkids = prompt('Do I have any grandkids?');
+var haveGrandkids = prompt('Do I have any grandkids?');
 
-// if(haveGrandkids.toUpperCase() === 'YES' || haveGrandkids.toUpperCase() === 'Y'){
-//   alert('I have 5 grandchildren. 3 granddaughters and 2 grandsons.');
-// } else {
-//   alert('Maybe I am older than I look?');
-// } console.log(haveGrandkids);
+if(haveGrandkids.toUpperCase() === 'YES' || haveGrandkids.toUpperCase() === 'Y'){
+  alert('I have 5 grandchildren. 3 granddaughters and 2 grandsons.');
+} else {
+  alert('Maybe I am older than I look?');
+} console.log(haveGrandkids);
 
-// var careerChange = prompt('Coding is a career change from the Army. Did I retire?');
+var careerChange = prompt('Coding is a career change from the Army. Did I retire?');
 
-// if(careerChange.toUpperCase() === 'Yes' || careerChange.toUpperCase() === 'Y') {
-//   alert('You know me better than I thought.');
-// } else {
-//   alert('I retired after serving 1992-2012');
-// } console.log(careerChange);
+if(careerChange.toUpperCase() === 'Yes' || careerChange.toUpperCase() === 'Y') {
+  alert('You know me better than I thought.');
+} else {
+  alert('I retired after serving 1992-2012');
+} console.log(careerChange);
 
-// var leisureTime = prompt('Do I fish?');
+var leisureTime = prompt('Do I fish?');
 
-// if(leisureTime.toUpperCase() === 'YES' || leisureTime.toUpperCase() === 'Y'){
-//   alert('Even when I don\'t get a bite my time on the water is the best.');
-// } else {
-//   alert('I don\'t catch, but I do fish.');
-// } console.log(leisureTime);
+if(leisureTime.toUpperCase() === 'YES' || leisureTime.toUpperCase() === 'Y'){
+  alert('Even when I don\'t get a bite my time on the water is the best.');
+} else {
+  alert('I don\'t catch, but I do fish.');
+} console.log(leisureTime);
 
-// var likesGameOfThrones = prompt('Do I like Game Of Thrones?');
+var likesGameOfThrones = prompt('Do I like Game Of Thrones?');
 
-// if(likesGameOfThrones.toUpperCase() === 'YES' || likesGameOfThrones.toUpperCase() === 'Y'){
-//   alert('Trick question. I have only seen the first episode.');
-// } else {
-//   alert('Trick question. I have only seen the first episode.');
-// } console.log(likesGameOfThrones);
+if(likesGameOfThrones.toUpperCase() === 'YES' || likesGameOfThrones.toUpperCase() === 'Y'){
+  alert('Trick question. I have only seen the first episode.');
+} else {
+  alert('Trick question. I have only seen the first episode.');
+} console.log(likesGameOfThrones);
 
 //6th Question
 
